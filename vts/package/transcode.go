8359347b83b3298ecd7a986eeef47235aa81c620
xSaCh <samarthchauhan7@gmail.com>
2025-01-05 02:20:08 +0530

    allow multi resoln

--- vts/package/transcode.go
+++ vts/package/transcode.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"sync"
 )
 
 type Transcoder interface {
@@ -16,16 +17,33 @@ type FFMpegTranscoder struct {
 }
 
 func (t *FFMpegTranscoder) Transcode(task common.MqTask) error {
-	// ffmpeg -i input.mp4 -c:v libx264 -c:a aac -strict experimental -b:a 192k -b:v 400k -f mp4 output.mp4
-	newFile := fmt.Sprintf("%s_q.mp4", task.Key)
-	cmd := exec.Command("ffmpeg", "-i", task.Key, "-c:v", "libx264", "-c:a", "aac", "-strict", "experimental", "-b:a", "192k", "-b:v", "400k", "-f", "mp4", newFile)
-	_ = newFile
+	var wg sync.WaitGroup
+	for _, resln := range task.Resolutions {
+		wg.Add(1)
+		go func() error {
+			defer wg.Done()
+			log.Printf("[Debug] compressing %s with resolution %d\n", task.Key, resln)
+			err := t.compress(task.Key, fmt.Sprintf("%s_%d.mp4", task.Key, resln), resln)
+			if err != nil {
+				return err
+			}
+			return nil
+		}()
+	}
+	wg.Wait()
+	return nil
+}
+
+func (t *FFMpegTranscoder) compress(inpFile, outFile string, resolution int) error {
+	// ffmpeg -i v.mp4 -vf scale=2048:-2 v2.mp4
+
+	cmd := exec.Command("ffmpeg", "-i", inpFile, "-vf", fmt.Sprintf("scale=%d:-2", resolution), outFile, "-y")
 
 	errBuff := bytes.Buffer{}
 	cmd.Stderr = &errBuff
 	err := cmd.Run()
 	if err != nil {
-		log.Printf("Error %s\n", errBuff.String())
+		log.Printf("[Error] for i: %s o: %s with r: %d: %s\n", inpFile, outFile, resolution, errBuff.String())
 		return err
 	}
 	return nil
